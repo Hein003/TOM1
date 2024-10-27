ec18a34928b8178b4ec5a377823dd57f88d208af
Hein003 <heinkheink12@gmail.com>
2024-10-27 14:36:09 +1100

    Create ErrorViewModel.cs

---
+++ Tom/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tom.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
