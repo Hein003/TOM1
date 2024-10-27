d1690319a57942565d7f73a9ac4fdcf87ffab563
Hein003 <heinkheink12@gmail.com>
2024-10-27 14:36:04 +1100

    Create BirthdayController.cs

---
+++ Tom/Controllers/BirthdayController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tom.Controllers
+{
+    public class BirthdayController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
